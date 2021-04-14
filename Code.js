66d6995c190b2246fdd270123537119566384251
Sergio Vega <hekinky@MacBook-Pro.local>
2021-04-14 15:50:36 -0600

    [I]

--- Code.js
+++ Code.js
@@ -12,3 +12,15 @@ function fakeBin(x){
     }
     return res
   }
+
+  //Build a function that returns an array of integers from n to 1 where n>0.
+
+  const reverseSeq = n => {
+  
+    let arr = []
+    
+    for (let i = n; i > 0; i--) {
+      arr.push(i)
+    }
+    return arr
+  };
